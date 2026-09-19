f6bbc1d79658c6ffdf5437d574debe7891d42ca6
jewelsdiagnostics-art <jewelsdiagnostics-art@users.noreply.github.com>
2026-09-19 22:39:44 +0200

    Bump Android release to 1.1.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.dalhousie.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.1.2"
+        versionCode = 9
+        versionName = "1.1.3"
     }
 
     buildTypes {
